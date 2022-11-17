fd041ed4a22d92239d98a84d7bd883a889364f6d
Dinh Ngoc Khanh <dinhngockhanh16d2@gmail.com>
2022-11-17 20:46:18 +0700

    update code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 function main() {
+  const eleFormRegister = document.querySelector('.js-form-register')
+  const eleBtnReset = document.querySelector('.js-btn-reset')
+  const eleInputUsername = document.querySelector('#username')
+  const eleInputPassword = document.querySelector('#password')
+  const eleInputFirstName = document.querySelector('#firstName')
+  const eleInputLastName = document.querySelector('#lastName')
 
+  function handleResetForm() {
+    eleFormRegister.reset()
+  }
+
+  eleBtnReset.addEventListener('click', (event) => {
+    event.preventDefault()
+    handleResetForm()
+  })
+
+  eleFormRegister.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const formValue = {
+      username: eleInputUsername.value,
+      passwort: eleInputPassword.value,
+      firstName: eleInputFirstName.value,
+      lastName: eleInputLastName.value,
+      gender: document.querySelector('[name="gender"]:checked')?.value
+    }
+
+    console.log(formValue);
+  })
 }
 
 main()
