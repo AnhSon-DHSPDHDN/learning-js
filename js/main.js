6e5a2eeae097e7a1c0f8bcdbc5bf2b2055328f65
Dinh Ngoc Khanh <dinhngockhanh16d2@gmail.com>
2022-11-22 20:41:12 +0700

    js/app_count_down

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 function main() {
-  alert('Welcome to Javascript')
+  const eleResultCountDown = document.querySelector('.js-result-count-down')
+  const newYearTime = new Date(2023, 0, 1)
+
+  setInterval(() => {
+    const currentTime = new Date()
+    let timeRemaining = Math.floor((newYearTime.getTime() - currentTime.getTime()) / 1000)
+    const dayRemain = Math.floor(timeRemaining / 60 / 60 / 24)
+
+    // Tính lại thời gian còn lại
+    timeRemaining = timeRemaining - dayRemain * 24 * 60 * 60
+    const hourRemain = Math.floor(timeRemaining / 60 / 60)
+
+    // Tính lại thời gian còn lại
+    timeRemaining = timeRemaining - hourRemain * 60 * 60
+    const minuteRemain = Math.floor(timeRemaining / 60)
+
+    // Tính lại thời gian còn lại
+    timeRemaining = timeRemaining - minuteRemain * 60
+    const secondRemain = timeRemaining
+
+    eleResultCountDown.textContent = `
+      Còn ${dayRemain} ngày, ${hourRemain} giờ, ${minuteRemain} phút, ${secondRemain} giây là ăn tết!
+    `
+  }, 1000)
 }
 
 main()
