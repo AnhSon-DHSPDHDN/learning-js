0d6e95910efcbf3b56797379be5547c720ea0c1f
Dinh Ngoc Khanh <dinhngockhanh16d2@gmail.com>
2022-12-01 19:09:41 +0700

    OOP in JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
-function main() {
-  alert('Welcome to Javascript')
+class MotoBike {
+  constructor(name, brand, maxSpeed, color, cc) {
+    this.name = name;
+    this.brand = brand;
+    this.maxSpeed = maxSpeed;
+    this.color = color;
+    this.cc = cc;
+  }
+
+  netbo() {
+    console.log('Khói trắng')
+  }
 }
 
-main()
+class MotoHonda extends MotoBike {
+  constructor(name, brand, maxSpeed, color, cc, price) {
+    super(name, brand, maxSpeed, color, cc, price);
+    this.price = price
+  }
+
+  dongCo = ''
+  hopSo = ''
+
+  netbo() {
+    console.log('Phun lửa!')
+  }
+
+  setDongCo(dongCo) {
+    this.dongCo = dongCo
+  }
+
+  setHopSo(hopSo) {
+    this.hopSo = hopSo
+  }
+}
+
+function Yamaha(name, price) {
+  this.name = name
+  this.price = price
+}
