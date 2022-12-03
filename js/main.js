517a720b8bb1578fb1ec6651fbdaa62dfc7ebae8
Dinh Ngoc Khanh <dinhngockhanh16d2@gmail.com>
2022-12-03 20:26:03 +0700

    init management task

--- js/main.js
+++ js/main.js
@@ -1,5 +1,72 @@
+const keyLocalTaskList = 'k_t_l'
+const taskLists = localStorage.getItem(keyLocalTaskList)
+  ? JSON.parse(localStorage.getItem(keyLocalTaskList))
+  : [];
+const eleInputTaskName = document.querySelector('#task-name')
+const eleInputTaskDes = document.querySelector('#task-des')
+const eleInputPriority = document.querySelector('#task-priority')
+const eleBodyTaskList = document.querySelector('.js-body-tasklist')
+const eleBtnSubmit = document.querySelector('.js-submit-form');
+const eleBtnReset = document.querySelector('.js-btn-reset');
+const eleLeftForm = document.querySelector('.js-left-form');
+
+const handleAddTask = () => {
+  const task = {
+    id: new Date().getTime(),
+    taskName: eleInputTaskName.value,
+    taskDescription: eleInputTaskDes.value,
+    taskPriority: eleInputPriority.value,
+    createAt: new Date(),
+    isDone: false,
+  }
+
+  taskLists.push(task);
+  localStorage.setItem(keyLocalTaskList, JSON.stringify(taskLists))
+  handleReRenderTaskList()
+  handleResetForm()
+}
+
+const handleResetForm = (event) => {
+  !!event && event.preventDefault()
+  eleLeftForm.reset()
+}
+
+const handleMakeDoneTask = () => {
+
+}
+
+const handleDeleteTask = () => {
+
+}
+
+const handleReRenderTaskList = () => {
+  let html = ''
+  taskLists.forEach(task => {
+    html += `
+      <tr class="${task.isDone ? 'done-task' : ''}">
+        <td>${task.taskName}</td>
+        <td>${task.createAt}</td>
+        <td>${task.taskDescription}</td>
+        <td>${task.taskPriority}</td>
+        <td>
+          <button>Done</button>
+          <button>Del</button>
+        </td>
+      </tr>
+    `
+  })
+
+  eleBodyTaskList.innerHTML = html
+}
+
 function main() {
-  alert('Welcome to Javascript')
+  handleReRenderTaskList()
+  eleBtnSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    handleAddTask()
+  })
+
+  eleBtnReset.addEventListener('click', handleResetForm)
 }
 
 main()
