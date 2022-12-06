e13dd0a241bdf22c4a2cc9f9632a6796757a3d80
Dinh Ngoc Khanh <dinhngockhanh16d2@gmail.com>
2022-12-06 19:53:02 +0700

    Example closure, callback, recursion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,54 @@
-function main() {
-  alert('Welcome to Javascript')
+function createCounter() {
+  let counter = 0;
+
+  function increCounter() {
+    counter = counter + 1
+    return counter
+  }
+
+  return increCounter
 }
 
-main()
+// const count1 = createCounter()
+// console.log(count1()); // 1
+// console.log(count1()); // 2
+// console.log(count1()); // 3
+
+// const count2 = createCounter()
+// console.log(count2()); // 1
+// console.log(count2()); // 2
+// console.log(count2()); // 3
+
+function hihi() {
+  console.log('hihi');
+}
+
+function haha() {
+  console.log('haha');
+}
+
+function demoCb(callback) {
+  callback()
+}
+
+function printSomething(something) {
+  console.log(something)
+}
+
+function sum(a, b, callback) {
+  const result = a + b
+  callback(result)
+}
+
+// sum(4, 5, printSomething)
+
+// demoCb(hihi)
+// demoCb(haha)
+
+function factorial(number) {
+  if (number === 1) return 1;
+
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(5));
